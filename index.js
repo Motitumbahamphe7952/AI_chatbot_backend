06f7cabf05cccf2c28b3486d36f1ec6038869d23
Nikhil Limbu <nikhillimbu918@gmail.com>
2026-01-24 20:39:32 +0545

    comment out the listen event

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 export default app;
